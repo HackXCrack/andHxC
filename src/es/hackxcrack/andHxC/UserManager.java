7c2062cde6a45086cde66f81b95442cd07fec7d8
z0mw33d <dpalma145@hotmail.com>
2012-11-30 18:55:39 +0100

    Exploracion (muy) basica del foro

--- src/es/hackxcrack/andHxC/UserManager.java
+++ src/es/hackxcrack/andHxC/UserManager.java
@@ -15,12 +15,14 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.entity.StringEntity;
 
+import android.os.AsyncTask;
+
 
 /**
  * Gestiona la sesión del usuario, implementa la toma de cookie, el login y el logout.
  */
-public class UserManager {
-
+public class UserManager extends AsyncTask<String, Void, Integer>{
+	
     /** Url del que tomar la cookie. */
     private final static String GET_NEW_COOKIE_URL = "http://www.hackxcrack.es/forum/";
 
@@ -37,7 +39,84 @@ public class UserManager {
 
     public UserManager(){}
 
+    @Override	
+	protected Integer doInBackground(String...args) {
+        // Se hace necesaria una cookie
+        //  ...o quizá no, no lo probé :P
+        try {
+            this.cookie = UserManager.getNewCookie();
+        } catch(IOException e) {
+            return -1;
+        }
 
+        if (this.cookie == null) {
+            return -1;
+        }
+
+
+        // Se hace la petición al sistema de login
+        boolean correctUserPass = false;
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpPost request = new HttpPost(LOGIN_URL);
+
+            request.addHeader("Cookie", this.cookie);
+            request.addHeader("Content-type", "application/x-www-form-urlencoded");
+
+            request.setEntity(new StringEntity("user=" + args[0]
+                                               + "&passwrd=" + args[1]
+                                               + "&openid_identifier=&cookielength=600&hash_password="));
+
+            HttpResponse response = httpclient.execute(request);
+
+            // Si responde redireccionando es que fue bien ^^
+            Header locationHeader = response.getLastHeader("location");
+            if (locationHeader != null) {
+                correctUserPass = true;
+                this.userName = args[0];
+
+                // Y solo queda cojer las cookies
+                Header[] cookieHeaders = response.getHeaders("set-cookie");
+                Vector<String> cookies = new Vector<String>();
+                this.cookie = "";
+
+                // Y reunir los cachitos
+                for (Header header: cookieHeaders) {
+                    String[] slices = header.getValue().split(";");
+
+                    if (slices.length > 0) {
+                        if (! cookies.contains(slices[0])) {
+                            cookies.add(slices[0]);
+                            if (this.cookie != "") {
+                                this.cookie += ";";
+                            }
+                            this.cookie += slices[0];
+                        }
+                    }
+                }
+            } else {
+                this.cookie = null;
+            }
+
+            // Excepciones que se pueden dar, simplemente las ignoramos
+        } catch (UnsupportedEncodingException encodingException) {
+        } catch (IOException ioException) {
+
+        } finally {
+            // Cerramos la conexión para asegurarnos de no malgastar recursos
+            httpclient.getConnectionManager().shutdown();
+        }
+
+        return 0;
+	}
+    
+    protected void onPostExecute(Integer correctUserPass){
+    	if ( correctUserPass == 0 )
+    		System.out.println("LOGEADO");
+    	else
+    		System.out.println("NO LOGEADO");
+    }
+        
     /** Devuelve la cookie. */
     public String getCookie(){
         return this.cookie;
@@ -95,11 +174,11 @@ public class UserManager {
      * @return boolean True si el login ha sido exitoso.
      *
      */
-    public boolean login(String username, String password){
+    /*public boolean login(String username, String password){
         // Se hace necesaria una cookie
         //  ...o quizá no, no lo probé :P
         try {
-            this.cookie = this.getNewCookie();
+            this.cookie = UserManager.getNewCookie();
         } catch(IOException e) {
             return false;
         }
@@ -163,7 +242,7 @@ public class UserManager {
         }
 
         return correctUserPass;
-    }
+    }*/
 
 
     /**
